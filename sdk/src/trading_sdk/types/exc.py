f52d55968f7dec351a23797c9c583fa46234a63c
m4rs <marcel@marcelclaramunt.cat>
2025-10-04 00:56:12 +0200

    exc printing fix

--- sdk/src/trading_sdk/types/exc.py
+++ sdk/src/trading_sdk/types/exc.py
@@ -1,17 +1,24 @@
 class Error(Exception):
-  ...
+  def __str__(self):
+    args = self.args[0] if len(self.args) == 1 else ', '.join(self.args)
+    return f'{self.__class__.__name__}({args})'
 
 class NetworkError(Error):
-  ...
+  def __str__(self):
+    return super().__str__()
 
 class ValidationError(Error):
-  ...
+  def __str__(self):
+    return super().__str__()
 
 class UserError(Error):
-  ...
+  def __str__(self):
+    return super().__str__()
 
 class AuthError(Error):
-  ...
+  def __str__(self):
+    return super().__str__()
 
 class ApiError(Error):
-  ...
+  def __str__(self):
+    return super().__str__()
