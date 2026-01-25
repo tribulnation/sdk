d34741b1d7e2be419b92fb5a7ea8c1089ec46e24
m4rs <marcel@marcelclaramunt.cat>
2026-01-25 18:03:55 +0100

    Operation, Posting rename to Event, Flow

--- sdk/src/trading_sdk/reporting/transactions/__init__.py
+++ sdk/src/trading_sdk/reporting/transactions/__init__.py
@@ -1,9 +1,9 @@
 from .types import (
-  Transaction, Operation, Posting,
-  Fee, Trade, FutureTrade, SinglePostingOperation,
+  Transaction, Event, Flow,
+  Fee, Trade, FutureTrade, SingleEvent,
   Yield, Bonus, Funding, Settlement, Other,
   CryptoDeposit, FiatDeposit, CryptoWithdrawal, FiatWithdrawal,
   EthereumTransaction, ERC20Transfer,
 )
 from .sdk import Transactions
-from .matching import match_transactions, PostingMatcher
+from .matching import match_transactions, EventMatcher
--- sdk/src/trading_sdk/reporting/transactions/types.py
+++ sdk/src/trading_sdk/reporting/transactions/types.py
@@ -1,5 +1,5 @@
 from typing_extensions import Literal, TypeVar, Any, Sequence, Union, Generic
-from dataclasses import dataclass
+from dataclasses import dataclass, replace, field
 from decimal import Decimal
 from datetime import datetime
 
@@ -7,39 +7,41 @@ D = TypeVar('D', default=Any, covariant=True)
 D2 = TypeVar('D2', default=Any, covariant=True)
 
 @dataclass(kw_only=True, frozen=True)
-class Posting(Generic[D]):
-  Type = Literal[
+class Flow(Generic[D]):
+  Label = Literal[
     'trade', 'yield', 'fee', 'bonus', 'other', 'internal_transfer',
     'future_trade', 'settlement', 'settlement_fee', 'funding',
     'strategy_deposit', 'strategy_withdrawal',
     'crypto_deposit', 'fiat_deposit', 'crypto_withdrawal', 'fiat_withdrawal', 'withdrawal_fee',
     'ethereum_transfer', 'erc20_transfer',
   ]
-  
+  kind: Literal['currency', 'future', 'strategy'] | None = None
   asset: str
   change: Decimal
-  kind: Literal['currency', 'future', 'strategy']
-  type: Type
+  label: Label
   time: datetime
   price: Decimal | None = None
-  operationId: str | None = None
   details: D
 
 @dataclass(kw_only=True, frozen=True)
-class BaseOperation(Generic[D]):
+class BaseEvent(Generic[D]):
   id: str
   time: datetime
   details: D
 
   @property
-  def expected_postings(self) -> Sequence[Posting[D]]:
-    """Postings expected to be matched."""
+  def expected_flows(self) -> Sequence[Flow[D]]:
+    """Flows expected to be matched."""
     return []
 
   @property
-  def fixed_postings(self) -> Sequence[Posting[D]]:
-    """Postings not appearing in the postings log but that should be included in the transaction."""
+  def fixed_flows(self) -> Sequence[Flow[D]]:
+    """Flows not appearing in the flows list but that should be included in the event."""
     return []
+
+  @property
+  def flows(self) -> Sequence[Flow[D]]:
+    return list(self.expected_flows) + list(self.fixed_flows)
 
 
 @dataclass(frozen=True)
@@ -47,69 +49,71 @@ class Fee:
   amount: Decimal
   asset: str
 
+def sign(side: Literal['buy', 'sell']) -> Literal[1, -1]:
+  match side:
+    case 'buy': return 1
+    case 'sell': return -1
+    case _: raise ValueError(f'Unknown side: {side}')
+
 @dataclass(kw_only=True, frozen=True)
-class Trade(BaseOperation[D], Generic[D]):
-  type: Literal['trade'] = 'trade'
+class Trade(BaseEvent[D], Generic[D]):
+  type: Literal['trade'] = field(default='trade', init=False)
   base: str
   quote: str
   qty: Decimal
   """Base asset quantity."""
   price: Decimal
-  liquidity: Literal['MAKER', 'TAKER']
-  side: Literal['BUY', 'SELL']
+  liquidity: Literal['maker', 'taker']
+  side: Literal['buy', 'sell']
   fee: Fee | None = None
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
-    s = 1 if self.side == 'BUY' else -1
-    postings = [
-      Posting(
-        kind='currency',
+  def expected_flows(self) -> list[Flow[D]]:
+    s = sign(self.side)
+    flows = [
+      Flow(
         time=self.time,
-        type='trade',
+        label='trade',
         asset=self.base,
         change=s * self.qty,
         details=self.details,
       ),
-      Posting(
-        kind='currency',
+      Flow(
         time=self.time,
-        type='trade',
+        label='trade',
         asset=self.quote,
         change=-s * self.qty * self.price,
         details=self.details,
       ),
     ]
     if self.fee is not None:
-      postings.append(Posting(
-        kind='currency',
+      flows.append(Flow(
         time=self.time,
-        type='fee',
+        label='fee',
         asset=self.fee.asset,
         change=-self.fee.amount,
         details=self.details,
       ))
-    return postings
+    return flows
     
 
 @dataclass(kw_only=True, frozen=True)
-class FutureTrade(BaseOperation[D], Generic[D]):
-  type: Literal['future_trade'] = 'future_trade'
-  asset: str
+class FutureTrade(BaseEvent[D], Generic[D]):
+  type: Literal['future_trade'] = field(default='future_trade', init=False)
+  instrument: str
   size: Decimal
   price: Decimal
-  liquidity: Literal['MAKER', 'TAKER']
-  side: Literal['BUY', 'SELL']
+  liquidity: Literal['maker', 'taker']
+  side: Literal['buy', 'sell']
   fee: Fee | None = None
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
+  def expected_flows(self) -> list[Flow[D]]:
     if self.fee is not None:
       return [
-        Posting(
-          kind='currency',
+        Flow(
           time=self.time,
-          type='fee',
+          label='fee',
           asset=self.fee.asset,
           change=-self.fee.amount,
           details=self.details,
@@ -119,14 +123,13 @@ class FutureTrade(BaseOperation[D], Generic[D]):
       return []
 
   @property  
-  def fixed_postings(self) -> list[Posting[D]]:
-    s = 1 if self.side == 'BUY' else -1
+  def fixed_flows(self) -> list[Flow[D]]:
+    s = sign(self.side)
     return [
-      Posting(
-        kind='future',
+      Flow(
         time=self.time,
-        type='future_trade',
-        asset=self.asset,
+        label='future_trade',
+        asset=self.instrument,
         change=s*self.size,
         price=self.price,
         details=self.details,
@@ -134,18 +137,17 @@ class FutureTrade(BaseOperation[D], Generic[D]):
     ]
 
 @dataclass(kw_only=True, frozen=True)
-class SinglePostingOperation(BaseOperation[D], Generic[D]):
+class SingleEvent(BaseEvent[D], Generic[D]):
   asset: str
   qty: Decimal
-  type: Posting.Type
+  type: Flow.Label
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
+  def expected_flows(self) -> list[Flow[D]]:
     return [
-      Posting(
-        kind='currency',
+      Flow(
         time=self.time,
-        type=self.type,
+        label=self.type,
         asset=self.asset,
         change=self.qty,
         details=self.details,
@@ -153,86 +155,76 @@ class SinglePostingOperation(BaseOperation[D], Generic[D]):
     ]
 
   @classmethod
-  def of(cls, id: str, posting: Posting[D2]) -> 'Operation[D2]':
-    match posting.type:
+  def of(cls, id: str, flow: Flow[D2]) -> 'Event[D2]':
+    match flow.label:
       case 'yield': cls = Yield
       case 'bonus': cls = Bonus
       case 'funding': cls = Funding
       case 'settlement': cls = Settlement
-      case 'strategy_deposit' | 'strategy_withdrawal': cls = Strategy
-      case 'fee': cls = FeeOperation
+      case 'fee': cls = FeeEvent
       case 'internal_transfer': cls = InternalTransfer
       case 'other': cls = Other
-      case _: raise ValueError(f'Unknown operation type: {posting.type}')
-    return cls(id=id, time=posting.time, asset=posting.asset, qty=posting.change, type=posting.type, details=posting.details)
+      case _: raise ValueError(f'Unknown operation type: {flow.label}')
+    return cls(id=id, time=flow.time, asset=flow.asset, qty=flow.change, type=flow.label, details=flow.details)
 
 
 @dataclass(kw_only=True, frozen=True)
-class FeeOperation(SinglePostingOperation[D], Generic[D]):
-  type: Literal['fee'] = 'fee'
+class FeeEvent(SingleEvent[D], Generic[D]):
+  type: Literal['fee'] = field(default='fee', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class InternalTransfer(SinglePostingOperation[D], Generic[D]):
-  type: Literal['internal_transfer'] = 'internal_transfer'
+class InternalTransfer(SingleEvent[D], Generic[D]):
+  type: Literal['internal_transfer'] = field(default='internal_transfer', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Strategy(SinglePostingOperation[D], Generic[D]):
+class Strategy(SingleEvent[D], Generic[D]):
+  strategy: str
   type: Literal['strategy_deposit', 'strategy_withdrawal']
 
   @property
-  def fixed_postings(self) -> list[Posting[D]]:
+  def fixed_flows(self) -> list[Flow[D]]:
     return [
-      Posting(
-        kind='currency',
+      Flow(
         time=self.time,
-        type=self.type,
-        asset=self.asset,
-        change=self.qty,
-        details=self.details,
-      ),
-      Posting(
-        kind='strategy',
-        time=self.time,
-        type=self.type,
-        asset=self.asset,
+        label=self.type,
+        asset=self.strategy,
         change=-self.qty,
         details=self.details,
       )
     ]
 
+@dataclass(kw_only=True, frozen=True)
+class Yield(SingleEvent[D], Generic[D]):
+  type: Literal['yield'] = field(default='yield', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Yield(SinglePostingOperation[D], Generic[D]):
-  type: Literal['yield'] = 'yield'
+class Other(SingleEvent[D], Generic[D]):
+  type: Literal['other'] = field(default='other', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Other(SinglePostingOperation[D], Generic[D]):
-  type: Literal['other'] = 'other'
+class Bonus(SingleEvent[D], Generic[D]):
+  type: Literal['bonus'] = field(default='bonus', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Bonus(SinglePostingOperation[D], Generic[D]):
-  type: Literal['bonus'] = 'bonus'
+class Funding(SingleEvent[D], Generic[D]):
+  type: Literal['funding'] = field(default='funding', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Funding(SinglePostingOperation[D], Generic[D]):
-  type: Literal['funding'] = 'funding'
+class Settlement(SingleEvent[D], Generic[D]):
+  type: Literal['settlement'] = field(default='settlement', init=False)
 
 @dataclass(kw_only=True, frozen=True)
-class Settlement(SinglePostingOperation[D], Generic[D]):
-  type: Literal['settlement'] = 'settlement'
-
-@dataclass(kw_only=True, frozen=True)
-class CryptoDeposit(SinglePostingOperation[D], Generic[D]):
+class CryptoDeposit(SingleEvent[D], Generic[D]):
   """Deposit from a blockchain."""
-  type: Literal['crypto_deposit'] = 'crypto_deposit'
+  type: Literal['crypto_deposit'] = field(default='crypto_deposit', init=False)
   network: str
   tx_hash: str
   idx: int | None = None
   """Index of the event within the same transaction."""
 
 @dataclass(kw_only=True, frozen=True)
-class FiatDeposit(SinglePostingOperation[D], Generic[D]):
-  type: Literal['fiat_deposit'] = 'fiat_deposit'
+class FiatDeposit(SingleEvent[D], Generic[D]):
+  type: Literal['fiat_deposit'] = field(default='fiat_deposit', init=False)
   method: str
   fiat_currency: str
   """Fiat currency used to buy the asset."""
@@ -240,36 +232,34 @@ class FiatDeposit(SinglePostingOperation[D], Generic[D]):
   """Amount of the fiat currency used to buy the asset."""
 
 @dataclass(kw_only=True, frozen=True)
-class Withdrawal(SinglePostingOperation[D], Generic[D]):
+class Withdrawal(SingleEvent[D], Generic[D]):
   fee: Fee | None = None
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
-    postings = [
-      Posting(
-        kind='currency',
+  def expected_flows(self) -> list[Flow[D]]:
+    flows = [
+      Flow(
         time=self.time,
-        type=self.type,
+        label=self.type,
         asset=self.asset,
         change=-self.qty,
         details=self.details,
       )
     ]
     if self.fee is not None:
-      postings.append(Posting(
-        kind='currency',
+      flows.append(Flow(
         time=self.time,
-        type='withdrawal_fee',
+        label='withdrawal_fee',
         asset=self.fee.asset,
         change=-self.fee.amount,
         details=self.details,
       ))
-    return postings
+    return flows
 
 @dataclass(kw_only=True, frozen=True)
 class CryptoWithdrawal(Withdrawal[D], Generic[D]):
   """Withdrawal into a blockchain."""
-  type: Literal['crypto_withdrawal'] = 'crypto_withdrawal'
+  type: Literal['crypto_withdrawal'] = field(default='crypto_withdrawal', init=False)
   network: str
   tx_hash: str
   idx: int | None = None
@@ -279,7 +269,7 @@ class CryptoWithdrawal(Withdrawal[D], Generic[D]):
 
 @dataclass(kw_only=True, frozen=True)
 class FiatWithdrawal(Withdrawal[D], Generic[D]):
-  type: Literal['fiat_withdrawal'] = 'fiat_withdrawal'
+  type: Literal['fiat_withdrawal'] = field(default='fiat_withdrawal', init=False)
   method: str
   fiat_currency: str
   """Fiat currency received."""
@@ -288,73 +278,103 @@ class FiatWithdrawal(Withdrawal[D], Generic[D]):
 
 
 @dataclass(kw_only=True, frozen=True)
-class BaseEthereumTransaction(BaseOperation[D], Generic[D]):
-  from_address: str
+class BaseEthereumTransaction(BaseEvent[D], Generic[D]):
   tx_hash: str
-  # idx: int | None = None
-  # """Index of the event within the same transaction."""
+  from_address: str
+  to_address: str
   chain_id: int
   """Chain ID (see https://chainid.network/)"""
 
 @dataclass(kw_only=True, frozen=True)
 class EthereumTransaction(BaseEthereumTransaction[D], Generic[D]):
-  type: Literal['ethereum_transfer'] = 'ethereum_transfer'
-  to_address: str
+  type: Literal['ethereum_transfer'] = field(default='ethereum_transfer', init=False)
   value: Decimal
   """Value sent [ETH]"""
-  fee: Decimal
+  fee: Decimal | None = None
   """Fee paid [ETH]"""
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
-    return [
-      Posting(
-        kind='currency',
+  def expected_flows(self) -> list[Flow[D]]:
+    flows: list[Flow[D]] = []
+    if self.value:
+      flows.append(Flow(
         time=self.time,
-        type='ethereum_transfer',
+        label='ethereum_transfer',
         asset='ETH',
         change=self.value,
         details=self.details,
-      ),
-      Posting(
-        kind='currency',
+      ))
+    if self.fee is not None:
+      flows.append(Flow(
         time=self.time,
-        type='fee',
+        label='fee',  
         asset='ETH',
         change=-self.fee,
         details=self.details,
-      ),
-    ]
+      ))
+    return flows
 
 @dataclass(kw_only=True, frozen=True)
 class ERC20Transfer(BaseEthereumTransaction[D], Generic[D]):
-  type: Literal['erc20_transfer'] = 'erc20_transfer'
-  contract_address: str
-  recipient_address: str
-  value: Decimal
-  """Value sent [Token]"""
+  @dataclass
+  class Transfer:
+    sender_address: str
+    recipient_address: str
+    contract_address: str
+    value: Decimal
+    """Value sent [Token]"""
+    direction: Literal['IN', 'OUT']
+
+  type: Literal['erc20_transfer'] = field(default='erc20_transfer', init=False)
+  fee: Decimal | None = None
+  """Fee paid [ETH]"""
+  transfers: Sequence[Transfer]
 
   @property
-  def expected_postings(self) -> list[Posting[D]]:
-    return [
-      Posting(
-        kind='currency',
+  def expected_flows(self) -> list[Flow[D]]:
+    flows = [
+      Flow(
         time=self.time,
-        type='erc20_transfer',
-        asset=self.contract_address,
-        change=self.value,
+        label='erc20_transfer',
+        asset=transfer.contract_address,
+        change=transfer.value * (1 if transfer.direction == 'IN' else -1),
         details=self.details,
       )
+      for transfer in self.transfers
     ]
+    if self.fee:
+      flows.append(Flow(
+        time=self.time,
+        label='fee',
+        asset='ETH',
+        change=-self.fee,
+        details=self.details,
+      ))
+    return flows
 
-Operation = Union[
+Event = Union[
   Trade[D], FutureTrade[D], Strategy[D], InternalTransfer[D],
-  Yield[D], Bonus[D], Funding[D], Settlement[D], Other[D],
+  Yield[D], Bonus[D], Funding[D], Settlement[D], Other[D], FeeEvent[D],
   CryptoDeposit[D], FiatDeposit[D], CryptoWithdrawal[D], FiatWithdrawal[D],
   EthereumTransaction[D], ERC20Transfer[D],
 ]
 
 @dataclass(kw_only=True, frozen=True)
 class Transaction(Generic[D]):
-  operation: Operation[D]
-  postings: Sequence[Posting[D]]
+  event: Event[D]
+  flows: Sequence[Flow[D]]
+
+  @property
+  def time(self):
+    return self.event.time
+
+  def replace_time(self, time: datetime) -> 'Transaction[D]':
+    return replace(
+      self, event=replace(self.event, time=time),
+      flows=[replace(p, time=time) for p in self.flows]
+    )
+
+  @classmethod
+  def single(cls, id: str, flow: Flow[D2]) -> 'Transaction[D2]':
+    event = SingleEvent.of(id, flow)
+    return cls(event=event, flows=[flow] + list(event.fixed_flows))
