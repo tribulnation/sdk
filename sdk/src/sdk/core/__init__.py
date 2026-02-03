cbc5797a2da53c064bbc454a866c56407e4bfe70
m4rs <marcel@marcelclaramunt.cat>
2026-02-03 13:45:39 +0100

    sdk instrumentations

--- sdk/src/sdk/core/__init__.py
+++ sdk/src/sdk/core/__init__.py
@@ -1,5 +1,5 @@
 from .exc import Error, NetworkError, ValidationError, UserError, AuthError, ApiError
 from .misc import Num, fmt_num
 from .networks import Network, is_network
-from .sdk import SDK, instrument
+from .sdk import SDK, instrument, exponential_retry, log
 from .util import round2tick, trunc2tick, Stream, ChunkedStream
--- sdk/src/sdk/core/sdk.py
+++ sdk/src/sdk/core/sdk.py
@@ -1,19 +1,114 @@
-from typing_extensions import TypeVar, Callable, Coroutine, AsyncGenerator, Protocol
+from typing_extensions import TypeVar, Callable, Coroutine, AsyncGenerator, Protocol, Generic, _ProtocolMeta
+from dataclasses import dataclass
+import asyncio
+import inspect
 
+T = TypeVar('T')
 Fn = TypeVar('Fn', bound=Callable[..., Coroutine|AsyncGenerator])
 
-class SDK(Protocol):
-  __sdk_methods__: list[str] = []
+@dataclass
+class Method(Generic[Fn]):
+  fn: Fn
+
+  def __get__(self, instance: object, owner: type) -> Fn:
+    return self.fn.__get__(instance, owner)
+
+class SDKMeta(_ProtocolMeta):
+  def __init__(cls, name, bases, dct):
+    super().__init__(name, bases, dct)
+    cls.__sdk_methods__ = {
+      k for k, v in dct.items() if isinstance(v, Method)
+    }
+    for base in bases:
+      if (methods := getattr(base, '__sdk_methods__', None)) is not None:
+        cls.__sdk_methods__.update(methods)
+
+class SDK(Protocol, metaclass=SDKMeta):
+  __sdk_methods__: set[str]
 
   @classmethod
   def method(cls, fn: Fn) -> Fn:
-    cls.__sdk_methods__.append(fn.__name__)
-    return fn
+    return Method(fn) # type: ignore
 
-S = TypeVar('S', bound=SDK)
-
-def instrument(sdk: S, mapper: Callable[[Fn], Fn]) -> S:
-  for attr in sdk.__sdk_methods__:
-    wrapped = mapper(getattr(sdk, attr))
-    setattr(sdk, attr, wrapped)
+def instrument(sdk: T, *mappers: Callable[[Fn], Fn]) -> T:
+  if (methods := getattr(sdk, '__sdk_methods__', None)) is not None:
+    for attr in methods:
+      if (fn := getattr(sdk, attr)) is not None:
+        for mapper in mappers:
+          fn = mapper(fn)
+        setattr(sdk, attr, fn)
+  for attr, value in list(sdk.__dict__.items()):
+    if getattr(value, '__sdk_methods__', None) is not None:
+      instrument(value, *mappers)
   return sdk
+
+E = TypeVar('E', bound=Exception)
+
+class RetryLogger(Protocol, Generic[E]):
+  def __call__(self, exception: E, *, retries: int, delay: float):
+    ...
+
+def default_retry_logger(exception: E, *, retries: int, delay: float):
+  print(f'Exponential retry [{retries=}, {delay=:.2f}s]. Exception:', exception)
+
+def exponential_retry(
+  *exceptions: type[E],
+  max_retries: int | None = None, base_delay: float = 1.0,
+  max_delay: float | None = None, log: RetryLogger[E] | None = default_retry_logger
+) -> Callable[[Fn], Fn]:
+    def exponential_retry_wrapper(fn: Fn) -> Fn:
+      if inspect.iscoroutinefunction(fn):
+        async def exponential_retry_wrapped(*args, **kwargs):
+          retries = 0
+          while True:
+            try:
+              return await fn(*args, **kwargs)
+            except exceptions as e:
+              if max_retries is not None and retries >= max_retries:
+                raise e
+              retries += 1
+              delay = base_delay * 2**retries
+              if max_delay is not None and delay > max_delay:
+                delay = max_delay
+              if log is not None:
+                log(e, retries=retries, delay=delay)
+              await asyncio.sleep(delay)
+        return exponential_retry_wrapped
+
+      else:
+        return fn
+
+    return exponential_retry_wrapper
+
+class Logger(Protocol):
+  def __call__(self, function: Fn, args: tuple, kwargs: dict):
+    ...
+
+def default_logger(function: Fn, args: tuple, kwargs: dict):
+  print(f'Calling {function.__name__} with {args=}, {kwargs=}')
+
+def log(logger: Logger = default_logger) -> Callable[[Fn], Fn]:
+  def log_wrapper(fn: Fn) -> Fn:
+    if inspect.iscoroutinefunction(fn):
+      async def log_wrapped(*args, **kwargs):
+        logger(fn, args, kwargs)
+        return await fn(*args, **kwargs)
+      return log_wrapped
+
+    elif inspect.isasyncgenfunction(fn):
+      async def log_wrapped(*args, **kwargs):
+        logger(fn, args, kwargs)
+        async for x in fn(*args, **kwargs):
+          yield x
+      return log_wrapped
+
+    elif inspect.isfunction(fn):
+      def log_wrapped(*args, **kwargs):
+        logger(fn, args, kwargs)
+        return fn(*args, **kwargs)
+      return log_wrapped
+
+    else:
+      raise ValueError(f"Unexpected function type: {type(fn)}")
+
+  return log_wrapper
