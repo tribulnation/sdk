9f4431137cde7be5c1f43435167a032c2f8fbc02
m4rs <marcel@marcelclaramunt.cat>
2026-02-16 12:04:19 +0100

    mexc impl: rm old spot

--- impl/mexc/src/mexc_sdk/spot/market_streams/__init__.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass as _dataclass
-
-from tribulnation.sdk.market import MarketStreams as _MarketStreams
-from .depth import Depth
-from .trades import Trades
-
-@_dataclass
-class MarketStreams(_MarketStreams, Depth, Trades):
-  ...
--- impl/mexc/src/mexc_sdk/spot/user_streams/__init__.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass as _dataclass
-
-from tribulnation.sdk.market import UserStreams as _UserStreams
-from .my_trades import MyTrades
-
-@_dataclass
-class UserStreams(_UserStreams, MyTrades):
-  ...
