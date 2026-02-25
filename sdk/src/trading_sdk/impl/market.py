a4ba69aafe83867bb9c8a0416bd9cd6139850705
m4rs <marcel@marcelclaramunt.cat>
2026-02-25 10:27:15 +0100

    sdk: rm async cache from load_market

--- sdk/src/trading_sdk/impl/market.py
+++ sdk/src/trading_sdk/impl/market.py
@@ -1,64 +1,51 @@
-from functools import cache
-from async_lru import alru_cache
 from trading_sdk import Market
 from mexc_sdk import MEXC
 from hyperliquid_sdk import Hyperliquid
 from dydx_sdk import DYDX
 
-@alru_cache
 async def dydx_sdk(mnemonic: str | None = None):
   return await DYDX.connect(mnemonic=mnemonic)
 
-@alru_cache
 async def dydx_market(market: str, *, subaccount: int = 0, mnemonic: str | None = None):
   dydx_client = await dydx_sdk(mnemonic)
   return await dydx_client.market(market, subaccount=subaccount)
 
-@cache
 def mexc_sdk(api_key: str | None = None, api_secret: str | None = None):
   return MEXC.new(api_key, api_secret)
 
-@alru_cache
 async def mexc_spot(instrument: str, *, api_key: str | None = None, api_secret: str | None = None):
   mexc = mexc_sdk(api_key, api_secret)
   return await mexc.spot(instrument)
 
-@alru_cache
 async def mexc_perp(instrument: str, *, api_key: str | None = None, api_secret: str | None = None):
   mexc = mexc_sdk(api_key, api_secret)
   return await mexc.perp(instrument)
 
-@cache
 def hyperliquid_sdk(address: str | None = None, *, wallet: str | None = None):
   return Hyperliquid.new(address=address, wallet=wallet)
 
-@alru_cache
 async def hyperliquid_spot(address: str | None = None, *, wallet: str | None = None):
-  hyperliquid = hyperliquid_sdk(address, wallet)
+  hyperliquid = hyperliquid_sdk(address, wallet=wallet)
   return await hyperliquid.spot()
 
-@alru_cache
 async def hyperliquid_spot_market(base: str, quote: str, index: int, *, address: str | None = None, wallet: str | None = None):
-  spot = await hyperliquid_spot(base, quote, address=address, wallet=wallet)
+  spot = await hyperliquid_spot(address=address, wallet=wallet)
   market = spot.spot(index)
   if market.base_name != base or market.quote_name != quote:
     raise ValueError(f'Expected market {base}/{quote} at index {index}, got {market.base_name}/{market.quote_name}')
   return market
 
 
-@alru_cache
 async def hyperliquid_perp(dex: str | None = None, *, address: str | None = None, wallet: str | None = None):
-  hyperliquid = hyperliquid_sdk(address, wallet)
+  hyperliquid = hyperliquid_sdk(address, wallet=wallet)
   return await hyperliquid.perp(dex)
 
-@alru_cache
 async def hyperliquid_perp_market(base: str, dex: str | None = None, *, address: str | None = None, wallet: str | None = None):
-  perp = await hyperliquid_perp(base, dex, address=address, wallet=wallet)
+  perp = await hyperliquid_perp(dex, address=address, wallet=wallet)
   if dex:
     base = f'{dex}:{base}'
   return perp.find(base)
 
-@alru_cache
 async def load_market(id: str) -> Market:
   """Load market SDK from ID.
   
