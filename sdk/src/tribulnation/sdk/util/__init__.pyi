20156416427df3d925d7b622e16098102836a6c8
m4rs <marcel@marcelclaramunt.cat>
2026-05-07 12:27:52 +0200

    sdk rm old csv util

--- sdk/src/tribulnation/sdk/util/__init__.pyi
+++ sdk/src/tribulnation/sdk/util/__init__.pyi
@@ -1,9 +1,6 @@
 from .numbers import Num, fmt_num, round2tick, trunc2tick, ceil2tick
-from .csv import validate_schema, ensure_datetime, Schema
-from . import csv
 
 __all__ = [
   'Num', 'fmt_num',
   'round2tick', 'trunc2tick', 'ceil2tick',
-  'validate_schema', 'ensure_datetime', 'Schema', 'csv',
 ]
