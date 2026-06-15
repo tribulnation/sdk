517e2e85ca3b7e176571ad99d1f389766125195b
Marcel <marcel@tribulnation.com>
2026-06-15 16:13:26 +0000

    sdk upgrade Snapshot model

--- sdk/src/tribulnation/sdk/reporting/models.py
+++ sdk/src/tribulnation/sdk/reporting/models.py
@@ -4,21 +4,24 @@ from datetime import datetime
 from decimal import Decimal
 import pydantic
 
-class Balance(pydantic.BaseModel):
-  qty: Decimal
-  kind: Literal['currency', 'future', 'strategy']
-  avg_price: Decimal | None = None
+class Position(pydantic.BaseModel):
+  size: Decimal
+  """Position size (of base units, positive or negative)"""
+  avg_price: Decimal
   """Average entry price"""
+
+  @classmethod
+  def merge(cls, positions: list['Position']) -> 'Position':
+    size = sum(p.size for p in positions)
+    if size == 0:
+      return cls(size=Decimal(0), avg_price=Decimal(0))
+    avg_price = sum(p.size * p.avg_price for p in positions) / size
+    return cls(size=size, avg_price=avg_price)
 
 class Snapshot(pydantic.BaseModel):
   time: pydantic.AwareDatetime
-  balances: dict[str, Balance]
-
-  def amount(self, asset: str) -> Decimal:
-    if (balance := self.balances.get(asset)):
-      return balance.qty
-    else:
-      return Decimal('0')
+  balances: dict[str, Decimal] = {}
+  positions: dict[str, Position] = {}
 
 ObservationType = Literal[
   'spot_trade',
