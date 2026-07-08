240eadf8316f7c42f565f5e4477282f47b2360ed
Marcel <marcel@tribulnation.com>
2026-07-08 16:35:37 +0000

    dydx fix wrap_exceptions

--- impl/dydx/src/tribulnation/dydx/core/exceptions.py
+++ impl/dydx/src/tribulnation/dydx/core/exceptions.py
@@ -23,11 +23,12 @@ def wrap_exceptions(fn):
       except core.Error as e:
         raise Error(*e.args) from e
 
-  elif inspect.isgeneratorfunction(fn):
+  elif inspect.isasyncgenfunction(fn):
     @wraps(fn)
     async def wrapper(*args, **kwargs): # type: ignore
       try:
-        return await fn(*args, **kwargs)
+        async for item in fn(*args, **kwargs):
+          yield item
       except core.NetworkError as e:
         raise NetworkError(*e.args) from e
       except core.ValidationError as e:
