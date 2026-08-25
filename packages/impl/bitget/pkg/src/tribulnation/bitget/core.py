03a6ee9f725f55b49b7e0e72e7be5cb94eb6a7fc
Marcel <marcel@tribulnation.com>
2026-08-25 15:30:51 +0000

    bitget core add UTA switch + simplify exceptions

--- packages/impl/bitget/pkg/src/tribulnation/bitget/core.py
+++ packages/impl/bitget/pkg/src/tribulnation/bitget/core.py
@@ -1,61 +1,58 @@
-from typing_extensions import Any, AsyncContextManager, Iterable, TypeVar
-from dataclasses import dataclass
-import inspect
+from typing_extensions import Any, AsyncContextManager, Iterable
+from dataclasses import dataclass, field
+import asyncio
 
 from tribulnation.sdk import SDK
-from tribulnation.sdk.core import ApiError, AuthError, Error, NetworkError, ValidationError
+from tribulnation.sdk.core import exception_wrapper
 
-from bitget import Bitget
-from bitget.core import exc
+from typed_bitget import Bitget
+from typed_bitget.core import exc
 
-Fn = TypeVar('Fn')
+wrap_exceptions = exception_wrapper()
 
-def wrap_exceptions(fn: Fn) -> Fn:
-  if inspect.iscoroutinefunction(fn):
-    async def awrapper(*args, **kwargs):
-      try:
-        return await fn(*args, **kwargs)
-      except exc.ApiError as e:
-        raise ApiError(*e.args) from e
-      except exc.AuthError as e:
-        raise AuthError(*e.args) from e
-      except exc.NetworkError as e:
-        raise NetworkError(*e.args) from e
-      except exc.ValidationError as e:
-        raise ValidationError(*e.args) from e
-      except exc.Error as e:
-        raise Error(*e.args) from e
-    return awrapper # type: ignore
-  elif inspect.isasyncgenfunction(fn):
-    async def agen_wrapper(*args, **kwargs):
-      try:
-        async for item in fn(*args, **kwargs):
-          yield item
-      except exc.ApiError as e:
-        raise ApiError(*e.args) from e
-      except exc.AuthError as e:
-        raise AuthError(*e.args) from e
-      except exc.NetworkError as e:
-        raise NetworkError(*e.args) from e
-      except exc.ValidationError as e:
-        raise ValidationError(*e.args) from e
-      except exc.Error as e:
-        raise Error(*e.args) from e
-    return agen_wrapper # type: ignore
-  else:
-    raise ValueError(f"Function {fn} is not a coroutine or generator function")
 
 @dataclass
 class SdkMixin(SDK):
   client: Bitget
+  uta: bool | None = None
+  """Is this account in UTA mode? If None, it will be auto-detected on first use."""
   validate: bool = True
+  _is_uta: bool | None = None
+  _is_uta_lock: asyncio.Lock = field(
+    default_factory=asyncio.Lock, init=False, repr=False
+  )
+
+  @SDK.method
+  async def determine_uta(self) -> bool:
+    try:
+      await self.client.uta.account.info()
+      return True
+    except exc.AccountModeMismatch:
+      return False
+
+  async def is_uta(self) -> bool:
+    if self.uta is not None:
+      return self.uta
+    elif self._is_uta is not None:  # short-circuit to avoid locking every time
+      return self._is_uta
+    else:
+      async with self._is_uta_lock:
+        if self._is_uta is None:
+          self._is_uta = await self.determine_uta()
+        return self._is_uta
 
   @classmethod
   def new(
-    cls, access_key: str | None = None, secret_key: str | None = None, passphrase: str | None = None, *,
-    validate: bool = True
+    cls,
+    access_key: str | None = None,
+    secret_key: str | None = None,
+    passphrase: str | None = None,
+    *,
+    validate: bool = True,
   ):
-    client = Bitget.new(access_key=access_key, secret_key=secret_key, passphrase=passphrase)
+    client = Bitget.new(
+      access_key=access_key, secret_key=secret_key, passphrase=passphrase
+    )
     return cls(client=client, validate=validate)
 
   def resources(self) -> Iterable[AsyncContextManager[Any]]:
